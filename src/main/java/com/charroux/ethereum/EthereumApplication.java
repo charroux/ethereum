d2ac35981f25fd9596cdcffd1623101f00edd0eb
charroux <benoit.charroux@gmail.com>
2023-06-26 11:03:50 +0200

    first commit

---
+++ src/main/java/com/charroux/ethereum/EthereumApplication.java
@@ -0,0 +1,13 @@
+package com.charroux.ethereum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EthereumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EthereumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charroux/ethereum/EthereumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charroux.ethereum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EthereumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
